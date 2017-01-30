9da0d23cfefe0274cccfdc4e9389a334a4ab7245
Radia Johnson <johnson.radia@gmail.com>
2017-01-30 16:56:46 -0500

    Update and rename GSEanalysisExamples.R to GSE_GAGEanalysisEg.R

--- GSE_GAGEanalysisEg.R
+++ GSE_GAGEanalysisEg.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------
-# This R script goes over the basics of pathway analysis in R
+# PART_2 R script: the basics of pathway analysis in R 
 # By Radia Johnson, Ph.D.
 # Date: Jan. 26th, 2017
 #--------------------------------------------------------------------
