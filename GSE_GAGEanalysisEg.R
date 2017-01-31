581ae2e168c5d88febab744a4a7e166c69660589
Radia Johnson <johnson.radia@gmail.com>
2017-01-31 13:39:13 -0500

    Update GSE_GAGEanalysisEg.R

--- GSE_GAGEanalysisEg.R
+++ GSE_GAGEanalysisEg.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------
-# PART_2 R script: the basics of pathway analysis in R 
+# PART_1 R script: the basics of pathway analysis in R 
 # By Radia Johnson, Ph.D.
 # Date: Jan. 26th, 2017
 #--------------------------------------------------------------------
